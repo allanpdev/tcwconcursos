670ac0aaa81309a6b2b0be5bf62728631038e544
Allan Pires <allanpires100@gmail.com>
2025-05-18 19:06:55 -0700

    18-05-2025

--- src/app/page.js
+++ src/app/page.js
@@ -18,20 +18,20 @@ export const metadata = {
 export default function Home() {
   return (
     <div>
-      <div className="flex justify-center items-center gap-1 bg-white text-blue-500 text-center font-[500] py-1">
+      {/* <div className="flex justify-center items-center gap-1 bg-white text-blue-500 text-center font-[500] py-1">
         <Image src="/warning.svg" width={18} height={18} alt="a"/>
         <span className="bg-gradient-to-b from-blue-800 to-blue-500 bg-clip-text text-transparent">Promoção válida até 30/05/2025</span>
-      </div>
+      </div> */}
 
       <Header/>
       
       <main className="bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat">
         <div className="flex flex-col items-center pt-7 pb-9 px-[32px]">
-          <Image src="/apostila.png" width={200} height={200} alt="foto-apostila"/>
+          <Image src="/apostila.png" width={185} height={185} alt="foto-apostila"/>
 
           <div className="pt-3 flex flex-col text-center text-white gap-3">
             <h1 className="font-semibold text-[1.2em] leading-[30px]">Você quer garantir sua vaga em um dos concursos mais aguardados do país?</h1>
-            <p className="text-[1.1em] font-[400] text-gray-300">Prepare-se para o concurso do INSS com o melhor material de estudos disponível na internet! Com ele, você pode estudar de maneira eficiente, e direcionada para as provas, com foco na sua aprovação!</p>
+            <p className="text-[1.05em] font-[400] text-gray-200">Prepare-se para o concurso do INSS com o melhor material de estudos disponível na internet! Com ele, você pode estudar de maneira eficiente, e direcionada para as provas, com foco na sua aprovação!</p>
           </div>
 
           <a href="" className="w-full h-[auto] mt-5 text-center bg-gradient-to-b from-[#0075ff] to-[#0050ad] uppercase text-white rounded-md py-3 text-[1em] font-semibold">Garantir com 30% off</a>
@@ -59,28 +59,28 @@ export default function Home() {
       <section id="vantagens" className="bg-white">
         <div className="flex flex-col p-6 gap-6 text-black">
           <div id="box-1" className="flex flex-col items-center text-center border px-6 py-8 rounded-md border-gray-300">
-            <Image src='/book-blue.svg' width={46} height={53}/>
-            <h2 className="mb-1 mt-2 text-[1.23em] font-semibold">Apostila Digital</h2>
+            <Image src='/book-blue.svg' width={40} height={53}/>
+            <h2 className="mb-2 mt-4 text-[1.23em] font-semibold">Apostila Digital</h2>
             <p className="text-[1em]">Oferecemos mapas mentais e plano de estudos para quem está começando do zero. Além de exercícios e milhares de questões gabaritadas pela Cebraspe.</p>
           </div>
 
           <div id="box-2" className="flex flex-col items-center text-center border px-6 py-8 rounded-md border-gray-300">
-            <Image src='/book-blue.svg' width={46} height={53}/>
-            <h2 className="mb-1 mt-2 text-[1.23em] font-semibold">Apostila Digital</h2>
+            <Image src='/book-blue.svg' width={40} height={53}/>
+            <h2 className="mb-2 mt-4 text-[1.23em] font-semibold">Apostila Digital</h2>
             <p className="text-[1em]">Oferecemos mapas mentais e plano de estudos para quem está começando do zero. Além de exercícios e milhares de questões gabaritadas pela Cebraspe.</p>
           </div>
 
           <div id="box-3" className="flex flex-col items-center text-center border px-6 py-8 rounded-md border-gray-300">
-            <Image src='/book-blue.svg' width={46} height={53}/>
-            <h2 className="mb-1 mt-2 text-[1.23em] font-semibold">Apostila Digital</h2>
+            <Image src='/book-blue.svg' width={40} height={53}/>
+            <h2 className="mb-2 mt-4 text-[1.23em] font-semibold">Apostila Digital</h2>
             <p className="text-[1em]">Oferecemos mapas mentais e plano de estudos para quem está começando do zero. Além de exercícios e milhares de questões gabaritadas pela Cebraspe.</p>
           </div>
         </div>
       </section>
 
-      <section id="garantia" className="flex flex-col items-center text-center px-8 py-10 text-white bg-[url('/garantia.png')]">
+      <section id="garantia" className="flex flex-col items-center text-center px-8 py-10 text-white bg-[#080B27]">
         <Image src='/estrela.png' width={158} height={53}/>
-        <h2 className="pt-4 pb-2 text-xl">Garantia de Satisfação</h2>
+        <h2 className="pt-4 pb-2 text-xl uppercase font-semibold">Garantia de Satisfação</h2>
         <p className="text-base">Se por algum motivo, você não estiver satisfeito com o nosso produto, basta com que solicite um reembolso, e a nossa equipe irá restituir 100% do seu dinheiro!</p>
       </section>
     </div>
